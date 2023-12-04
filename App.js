d3589bf8855f341f33360b5d43fed3d2a6a8fcb3
Ibnu-Faqri <ibnufaqri2003@gmail.com>
2023-12-04 22:38:51 +0700

    boiler plate

--- App.js
+++ App.js
@@ -7,5 +7,8 @@ app.use(express.json());
 const port = 4003;
 app.get("/", (req, res) => {
     res.status(200).json("HELLO WORLD");
-    console.log(`Server is running on port ${port}`);
   });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
