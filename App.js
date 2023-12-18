8ec9d2aa96049f861106a67a8d3c55b1dc22158b
farhantaufiks <farhantaufiks@gmail.com>
2023-12-18 21:03:05 +0700

    testing cron

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const routers = require("./routes/index");
 const app = express();
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 const errorHandling = require("./middlewares/errorHandling");
+const { cronUpdateStatus } = require("./controller/reservasiController");
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -12,9 +13,11 @@ app.use(errorHandling);
 
 const port = 4003;
 app.get("/", (req, res) => {
-    res.status(200).json("HELLO WORLD");
-  });
+  res.status(200).json("HELLO WORLD");
+});
+
+cronUpdateStatus();
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
