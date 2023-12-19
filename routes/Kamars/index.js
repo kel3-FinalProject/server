74959196d0ec1192073c2a6854fdfcbcd7d49b91
Ibnu-Faqri <ibnufaqri2003@gmail.com>
2023-12-19 21:43:02 +0700

    BE_done

--- routes/Kamars/index.js
+++ routes/Kamars/index.js
@@ -4,8 +4,8 @@ const authentication = require("../../middlewares/authentication");
 const isAdmin = require("../../middlewares/isAdmin");
 
 router.post("/", authentication, isAdmin, KamarController.addKamar);
-router.get("/", KamarController.getKamar);
-router.get("/:id", KamarController.getKamarById);
+router.get("/", authentication, KamarController.getKamar);
+router.get("/:id", authentication, KamarController.getKamarById);
 router.put("/:id", authentication, isAdmin, KamarController.updateKamar);
 router.delete("/:id", authentication, isAdmin, KamarController.deleteKamar);
 
